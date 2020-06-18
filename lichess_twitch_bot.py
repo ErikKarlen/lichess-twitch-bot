1cf6eeb34622d322702e630df3211eb722ca6204
Erik K <erik.karlen@gmail.com>
2020-06-19 00:11:01 +0200

    Convert tabs to spaces

--- lichess_twitch_bot.py
+++ lichess_twitch_bot.py
@@ -1,8 +1,8 @@
 """
-	COPYRIGHT INFORMATION
-	---------------------
+    COPYRIGHT INFORMATION
+    ---------------------
 Based on the twitch bot tutorial by Carberra.
-	https://github.com/Carberra/twitch-bot-tutorial/blob/master/twitch_tut.py
+    https://github.com/Carberra/twitch-bot-tutorial/blob/master/twitch_tut.py
 Some code in this file is licensed under the Apache License, Version 2.0.
     http://aws.amazon.com/apache2.0/
 """
@@ -16,39 +16,39 @@ OWNER = "naitmare"
 
 class LichessTwitchBot(SingleServerIRCBot):
 
-	def __init__(self):
-		self.HOST = "irc.chat.twitch.tv"
-		self.PORT = 6667
-		self.USERNAME = NAME.lower()
-		self.CLIENT_ID = "" # Fill in your client ID
-		self.TOKEN = "" # Fill in Token
-		self.CHANNEL = f"#{OWNER}"
+    def __init__(self):
+        self.HOST = "irc.chat.twitch.tv"
+        self.PORT = 6667
+        self.USERNAME = NAME.lower()
+        self.CLIENT_ID = "" # Fill in client ID
+        self.TOKEN = "" # Fill in Token
+        self.CHANNEL = f"#{OWNER}"
 
-		url = f"https://api.twitch.tv/kraken/users?login={self.USERNAME}"
-		headers = {"Client-ID": self.CLIENT_ID, "Accept": "application/vnd.twitchtv.v5+json"}
-		resp = get(url, headers=headers).json()
-		self.channel_id = resp["users"][0]["_id"]
-		print("init 1 finished")
+        url = f"https://api.twitch.tv/kraken/users?login={self.USERNAME}"
+        headers = {"Client-ID": self.CLIENT_ID, "Accept": "application/vnd.twitchtv.v5+json"}
+        resp = get(url, headers=headers).json()
+        self.channel_id = resp["users"][0]["_id"]
+        print("init 1 finished")
 
-		super().__init__([(self.HOST, self.PORT, f"oauth:{self.TOKEN}")], self.USERNAME, self.USERNAME)
-		print("init 2 finished")
+        super().__init__([(self.HOST, self.PORT, f"oauth:{self.TOKEN}")], self.USERNAME, self.USERNAME)
+        print("init 2 finished")
 
-	def on_welcome(self, cxn, event):
-		for req in ("membership", "tags", "commands"):
-			cxn.cap("REQ", f":twitch.tv/{req}")
-		print("welcome finished")
+    def on_welcome(self, cxn, event):
+        for req in ("membership", "tags", "commands"):
+            cxn.cap("REQ", f":twitch.tv/{req}")
+        print("welcome finished")
 
-		cxn.join(self.CHANNEL)
-		print("joined channel")
-		self.send_message("Now online.")
+        cxn.join(self.CHANNEL)
+        print("joined channel")
+        self.send_message("Now online.")
 
-	def on_pubmsg(self, cxn, event):
-		tags = {kvpair["key"]: kvpair["value"] for kvpair in event.tags}
-		user = {"name": tags["display-name"], "id": tags["user-id"]}
-		message = event.arguments[0]
+    def on_pubmsg(self, cxn, event):
+        tags = {kvpair["key"]: kvpair["value"] for kvpair in event.tags}
+        user = {"name": tags["display-name"], "id": tags["user-id"]}
+        message = event.arguments[0]
 
-		print(f"Message from {user['name']}: {message}")
+        print(f"Message from {user['name']}: {message}")
 
-	def send_message(self, message):
-		print("sending message")
-		self.connection.privmsg(self.CHANNEL, message)
+    def send_message(self, message):
+        print("sending message")
+        self.connection.privmsg(self.CHANNEL, message)
