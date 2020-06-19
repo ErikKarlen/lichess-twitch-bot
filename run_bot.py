a37126c148df12ce9bc58133830cbb3612b52a3b
Erik K <erik.karlen@gmail.com>
2020-06-20 00:59:45 +0200

    Split main file into multiple functions

--- run_bot.py
+++ run_bot.py
@@ -13,23 +13,37 @@ import yaml
 from lichess_twitch_bot import LichessTwitchBot
 
 
-def main(main_args):
+def parse_args(main_args):
     """
-    Main program function.
+    Parse arguments
     """
-
-    # Setup argument parser
     parser = ap.ArgumentParser(description="Starts a Lichess Twitch Bot")
     parser.add_argument("-c", "--configuration", type=str, help="configuration file", required=True)
     args = parser.parse_args(main_args[1:])
 
-    # Load configuration
-    configuration_path = Path(args.configuration)
+    return args
+
+def load_configuration(configuration_file):
+    """
+    Load program configuration
+    """
+    configuration_path = Path(configuration_file)
     with open(configuration_path, "r") as configuration_stream:
         try:
             configuration = yaml.safe_load(configuration_stream)
         except yaml.YAMLError as exc:
             print(exc)
+
+    return configuration
+
+
+def main(main_args):
+    """
+    Main program function.
+    """
+    # Basic setup
+    args = parse_args(main_args)
+    configuration = load_configuration(args.configuration)
 
     # Start bot
     bot = LichessTwitchBot(configuration["twitch"]["client_id"], configuration["twitch"]["token"])
