9b17b74e409da1889fce8e45be2eac81ba673b14
Erik K <erik.karlen@gmail.com>
2020-06-19 02:06:20 +0200

    Add argparser to run file

--- lichess_twitch_bot.py
+++ lichess_twitch_bot.py
@@ -1,10 +1,13 @@
-"""
+""" Lichess Twitch Bot
+
     COPYRIGHT INFORMATION
     ---------------------
 Based on the twitch bot tutorial by Carberra.
     https://github.com/Carberra/twitch-bot-tutorial/blob/master/twitch_tut.py
 Some code in this file is licensed under the Apache License, Version 2.0.
     http://aws.amazon.com/apache2.0/
+
+lichess_twitch_bot.py
 """
 
 from irc.bot import SingleServerIRCBot
@@ -15,22 +18,26 @@ OWNER = "naitmare"
 
 
 class LichessTwitchBot(SingleServerIRCBot):
-
-    def __init__(self):
+    def __init__(self, client_id, token):
         self.HOST = "irc.chat.twitch.tv"
         self.PORT = 6667
         self.USERNAME = NAME.lower()
-        self.CLIENT_ID = "" # Fill in client ID
-        self.TOKEN = "" # Fill in Token
+        self.CLIENT_ID = client_id
+        self.TOKEN = token
         self.CHANNEL = f"#{OWNER}"
 
         url = f"https://api.twitch.tv/kraken/users?login={self.USERNAME}"
-        headers = {"Client-ID": self.CLIENT_ID, "Accept": "application/vnd.twitchtv.v5+json"}
+        headers = {
+            "Client-ID": self.CLIENT_ID,
+            "Accept": "application/vnd.twitchtv.v5+json",
+        }
         resp = get(url, headers=headers).json()
         self.channel_id = resp["users"][0]["_id"]
         print("init 1 finished")
 
-        super().__init__([(self.HOST, self.PORT, f"oauth:{self.TOKEN}")], self.USERNAME, self.USERNAME)
+        super().__init__(
+            [(self.HOST, self.PORT, f"oauth:{self.TOKEN}")], self.USERNAME, self.USERNAME,
+        )
         print("init 2 finished")
 
     def on_welcome(self, cxn, event):
--- run_bot.py
+++ run_bot.py
@@ -1,9 +1,38 @@
+""" Run Bot
+
+Main run file for Lichess Twitch Bot.
+
+run_bot.py
+"""
+
 import sys
+import argparse as ap
+from pathlib import Path
+import yaml
 
 from lichess_twitch_bot import LichessTwitchBot
 
+
 def main(main_args):
-    bot = LichessTwitchBot()
+    """
+    Main program function.
+    """
+
+    # Setup argument parser
+    parser = ap.ArgumentParser(description="Starts a Lichess Twitch Bot")
+    parser.add_argument("-c", "--configuration", type=str, help="configuration file", required=True)
+    args = parser.parse_args(main_args[1:])
+
+    # Load configuration
+    configuration_path = Path(args.configuration)
+    with open(configuration_path, "r") as configuration_stream:
+        try:
+            configuration = yaml.safe_load(configuration_stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # Start bot
+    bot = LichessTwitchBot(configuration["twitch"]["client_id"], configuration["twitch"]["token"])
     bot.start()
 
 
